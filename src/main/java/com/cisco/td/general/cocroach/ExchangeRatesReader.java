413f127a3334f635e10d9a1eaef91ab8d74d084b
judas <jjudas@cisco.com>
2022-03-22 08:08:50 +0100

    CHOPIN-56079: use CsvReader in ExchangeRatesReader
    Former-commit-id: 5cc7bd31c29227d83c950d7e0a7ff576a78b434f

--- src/main/java/com/cisco/td/general/cocroach/ExchangeRatesReader.java
+++ src/main/java/com/cisco/td/general/cocroach/ExchangeRatesReader.java
@@ -1,68 +1,41 @@
 package com.cisco.td.general.cocroach;
 
+import com.cisco.td.ade.csv.CsvReader;
 import com.cognitivesecurity.commons.collections.MoreFluentIterable;
 import com.cognitivesecurity.commons.io.ByteSourceChain;
-import com.cognitivesecurity.commons.util.ExceptionUtils;
-import com.cognitivesecurity.commons.util.Literals;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-
-import java.util.List;
-import java.util.Map;
-
-import static com.cognitivesecurity.commons.util.Literals.mapEntry;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Value;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 
 public class ExchangeRatesReader {
-
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern("dd.MM.YYYY").withZoneUTC();
-
-
-    private final ObjectReader objectReader;
-
-    public ExchangeRatesReader() {
-        CsvMapper csvMapper = new CsvMapper();
-
-        CsvSchema schema = csvMapper.schemaFor(String[].class)
-                .withColumnSeparator('|')
-                .withoutQuoteChar()
-                .withoutEscapeChar();
-
-        this.objectReader = csvMapper.readerFor(String[].class).with(schema);
-    }
+    private final CsvReader csvReader = CsvReader.builder().separator('|').build();
 
     public TabularExchangeRateProvider parse(ByteSourceChain data) {
-        List<String> lines = data.fluentLines().toList();
-        String header = lines.get(0);
-        Map<String, Integer> itemMap = MoreFluentIterable.from((String[]) ExceptionUtils.wrapChecked(()->objectReader.readValue(header)))
-                .mapWithIndex((i, item) -> mapEntry(item, i))
-                .collect(Literals::mapCopy);
-
-        int datumIndex = itemMap.get("Datum");
-        int usdIndex = itemMap.get("1 USD");
-
-        Map<DateTime, Double> ratesTable = MoreFluentIterable.from(lines).skip(1).map(line -> {
-
-            try {
-                String[] values;
-                values = objectReader.readValue(line);
-                return mapEntry(
-                        DATE_FORMATTER.parseDateTime(values[datumIndex]),
-                        Double.parseDouble(values[usdIndex].replace(',','.'))
-                );
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }).collect(Literals::mapCopy);
-
-
-        return new TabularExchangeRateProvider(ratesTable);
+        return MoreFluentIterable.from(csvReader.readInputContainingHeader(data, Line.class))
+                .toFluentMap(
+                        line -> line.getDate().toDateTime(LocalTime.MIDNIGHT),
+                        line -> line.getRate().getAmount()
+                )
+                .convert(TabularExchangeRateProvider::new);
     }
 
+    @Value
+    private static class Line {
+        @JsonProperty("Datum")
+        @JsonFormat(pattern = "dd.MM.YYYY")
+        LocalDate date;
+        @JsonProperty("1 USD")
+        Money rate;
+    }
 
+    @Value
+    private static class Money {
+        private final double amount;
+
+        public static Money fromString(String input) {
+            return new Money(Double.parseDouble(input.replace(',', '.')));
+        }
+    }
 }
