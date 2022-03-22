70b53000a2c778622ec0bc14294759c0f3093304
judas <jjudas@cisco.com>
2022-03-22 14:06:49 +0100

    CHOPIN-56079: use NavigableMap
    Former-commit-id: dd9ea7eb03bb9c88387417f031895c9619deaa24

--- src/main/java/com/cisco/td/general/cocroach/TabularExchangeRateProvider.java
+++ src/main/java/com/cisco/td/general/cocroach/TabularExchangeRateProvider.java
@@ -1,29 +1,31 @@
 package com.cisco.td.general.cocroach;
 
 import com.cognitivesecurity.commons.io.ByteSources;
-import lombok.RequiredArgsConstructor;
+import com.google.common.collect.ImmutableSortedMap;
 import org.joda.time.LocalDate;
 
 import java.util.Map;
+import java.util.NavigableMap;
 
-@RequiredArgsConstructor
 public class TabularExchangeRateProvider implements ExchangeRateProvider {
-    private final Map<LocalDate, Double> knownRates;
+    private final NavigableMap<LocalDate, Double> knownRates;
 
-    public static TabularExchangeRateProvider hardcoded()  {
+    public TabularExchangeRateProvider(Map<LocalDate, Double> knownRates) {
+        this.knownRates = ImmutableSortedMap.copyOf(knownRates);
+    }
+
+    public static TabularExchangeRateProvider hardcoded() {
         return new ExchangeRatesReader()
                 .parse(ByteSources.fromResource(TabularExchangeRateProvider.class, "rates_2021.txt"));
     }
 
     @Override
     public double rateAt(LocalDate day) {
-        for (int i = 0; i < 10; i++) {
-            Double maybeValue = knownRates.get(day.minusDays(i));
-            if (maybeValue != null) {
-                return maybeValue;
-            }
+        Map.Entry<LocalDate, Double> maybeEntry = knownRates.floorEntry(day);
+        if (maybeEntry != null) {
+            return maybeEntry.getValue();
+        } else {
+            throw new IllegalArgumentException("can not find rate for " + day);
         }
-
-        throw new IllegalArgumentException("can not find rate for " + day);
     }
 }
