7ed206a113f60aad432264718fe754a3b3c9dfd7
judas <jjudas@cisco.com>
2022-03-22 14:10:55 +0100

    CHOPIN-56079: reformat
    Former-commit-id: 4fcecf464f44309cfa4557ed28bafe437963554c

--- src/main/java/com/cisco/td/general/cocroach/ReportGenerator.java
+++ src/main/java/com/cisco/td/general/cocroach/ReportGenerator.java
@@ -4,34 +4,38 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class ReportGenerator {
+
+    private final SalesReportPreparation salesReportPreparation = new SalesReportPreparation();
+    private final EsppReportPreparation esppReportPreparation = new EsppReportPreparation();
+    private final RsuReportPreparation rsuReportPreparation = new RsuReportPreparation();
+    private final DividentReportPreparation dividentReportPreparation = new DividentReportPreparation();
+
     public Report generateForYear(ParsedExport parsedExport, int year, ExchangeRateProvider exchangeRateProvider) {
         DateInterval interval = DateInterval.year(year);
 
         return new Report(
-                generateRsuReport(parsedExport, interval, exchangeRateProvider),
-                generateDividendReport(parsedExport, interval, exchangeRateProvider),
-                generateEsppReport(parsedExport, interval, exchangeRateProvider),
-                generateSalesReport(parsedExport, interval, exchangeRateProvider)
+                rsuReportPreparation.generateRsuReport(
+                        parsedExport.getRsuRecords(),
+                        interval,
+                        exchangeRateProvider
+                ),
+                dividentReportPreparation.generateDividendReport(
+                        parsedExport.getDividendRecords(),
+                        parsedExport.getTaxRecords(),
+                        parsedExport.getTaxReversalRecords(),
+                        interval,
+                        exchangeRateProvider
+                ),
+                esppReportPreparation.generateEsppReport(
+                        parsedExport.getEsppRecords(),
+                        interval,
+                        exchangeRateProvider
+                ),
+                salesReportPreparation.generateSalesReport(
+                        parsedExport.getSaleRecords(),
+                        interval,
+                        exchangeRateProvider
+                )
         );
-    }
-
-    private SalesReport generateSalesReport(ParsedExport parsedExport, DateInterval interval, ExchangeRateProvider exchangeRateProvider) {
-        SalesReportPreparation salesReportPreparation = new SalesReportPreparation();
-        return salesReportPreparation.generateSalesReport(parsedExport.getSaleRecords(), interval, exchangeRateProvider);
-    }
-
-    private EsppReport generateEsppReport(ParsedExport parsedExport, DateInterval interval, ExchangeRateProvider exchangeRateProvider) {
-        EsppReportPreparation esppReportPreparation = new EsppReportPreparation();
-        return esppReportPreparation.generateEsppReport(parsedExport.getEsppRecords(), interval, exchangeRateProvider);
-    }
-
-    private RsuReport generateRsuReport(ParsedExport parsedExport, DateInterval interval, ExchangeRateProvider exchangeRateProvider) {
-        RsuReportPreparation rsuReportPreparation = new RsuReportPreparation();
-        return rsuReportPreparation.generateRsuReport(parsedExport.getRsuRecords(), interval, exchangeRateProvider);
-    }
-
-    private DividendReport generateDividendReport(ParsedExport parsedExport, DateInterval interval, ExchangeRateProvider exchangeRateProvider) {
-        DividentReportPreparation dividentReportPreparation = new DividentReportPreparation();
-        return dividentReportPreparation.generateDividendReport(parsedExport.getDividendRecords(), parsedExport.getTaxRecords(), parsedExport.getTaxReversalRecords(), interval, exchangeRateProvider);
     }
 }
